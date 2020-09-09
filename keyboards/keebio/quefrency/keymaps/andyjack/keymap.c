ce466d1f9420df9a9fa4843eb09d29970a167440
Andy Jack <github@veracity.ca>
2020-09-09 11:58:53 -0400

    Tired of getting del/bksp wrong

--- keyboards/keebio/quefrency/keymaps/andyjack/keymap.c
+++ keyboards/keebio/quefrency/keymaps/andyjack/keymap.c
@@ -33,7 +33,7 @@ enum custom_keycodes { AJ_FN1 = SAFE_RANGE, AJ_FN2 };
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT_65_with_macro(
-    _______, KC_ESC,  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,             KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_DEL,  KC_BSPC, _______, \
+    _______, KC_ESC,  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,             KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_BSPC, KC_DEL,  \
     KC_F1,   KC_F2,   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_INS, \
     KC_F3,   KC_F4,   CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,             KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  KC_HOME, \
     KC_F5,   KC_F6,   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,             KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_END,  \
@@ -41,15 +41,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_L1] = LAYOUT_65_with_macro(
-    _______, _______, _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,            KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_BSPC, _______, \
-    MUT_IN,  _______, _______, _______, _______, KC_UP,   _______, _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+    _______, _______, _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,            KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_DEL,  _______, \
+    MUT_IN,  _______, _______, _______, _______, KC_UP,   _______, _______,          _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL, \
     MUT_OUT, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______,          _______, _______, _______, _______, _______, _______, _______, KC_PGUP, \
     MUT_MIC, _______, _______, _______, _______, _______, KC_BTN3, _______,          _______, _______, _______, _______, _______, _______, _______, KC_PGDN, \
     SCN_LCK, _______, _______, _______, _______, _______, KC_DEL,                    _______, KC_BSPC, _______, _______, _______, KC_MPRV, KC_MPLY, KC_MNXT
   ),
 
   [_L2] = LAYOUT_65_with_macro(
-    _______, _______, _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,            KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_BSPC, _______, \
+    _______, _______, _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,            KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______, \
     _______, _______, _______, _______, _______, KC_UP,   _______, _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, \
     _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______,          _______, _______, _______, _______, _______, _______, _______, _______, \
     _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______, _______, \
