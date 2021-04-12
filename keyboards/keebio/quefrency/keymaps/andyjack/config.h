0b6b18cb4dcd27999e39cfe03cc4013b324a8cff
Andy Jack <github@veracity.ca>
2021-04-12 10:45:09 -0400

    Adding led backlighting; removing rh encoder; wide backspace key
    - left encoder now controls backlighting and add some L1 backlight
      controls

--- keyboards/keebio/quefrency/keymaps/andyjack/config.h
+++ keyboards/keebio/quefrency/keymaps/andyjack/config.h
@@ -21,4 +21,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
+#define BACKLIGHT_LEVELS 24
+#define BACKLIGHT_BREATHING
+#define BACKLIGHT_PERIOD 10
+
 // #define USE_I2C
--- keyboards/keebio/quefrency/keymaps/andyjack/readme.md
+++ keyboards/keebio/quefrency/keymaps/andyjack/readme.md
@@ -1,4 +1,4 @@
 ## Customized from the `default65` keymap
 
 * 65-key layout, plus 10 left-side macro buttons
-* dual encoders
+* single encoder
